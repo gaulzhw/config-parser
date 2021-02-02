2118da07a5423062032ca37fc592bce2ff7528d0
Zhiwen Gao <zhiwen.gao@shopee.com>
2021-02-02 20:18:18 +0800

    fix no-ssl

--- params/server-options.go
+++ params/server-options.go
@@ -126,7 +126,7 @@ func getServerOptions() []ServerOption {
 		&ServerOptionWord{Name: "check"},
 		&ServerOptionWord{Name: "check-send-proxy"},
 		&ServerOptionWord{Name: "check-ssl"},
-		&ServerOptionWord{Name: "nossl"},
+		&ServerOptionWord{Name: "no-ssl"},
 		&ServerOptionWord{Name: "check-via-socks4"},
 		&ServerOptionWord{Name: "disabled"},
 		&ServerOptionWord{Name: "enabled"},
